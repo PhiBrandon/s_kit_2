78c1c76be83939ea7df84bcc3a1de824d9a84856
Brandon Phillips <phillipsbrandonit@gmail.com>
2024-05-26 09:17:35 -0600

    updated paginator

--- win/nice/views.py
+++ win/nice/views.py
@@ -2,26 +2,44 @@ from django.shortcuts import render
 from django.core.paginator import Paginator
 from .models import Observation
 import json
+
+
 # Create your views here.
 def base_view(request):
     return render(request, "base.html")
 
 
-
-
+# .filter(model="meta-llama/Llama-3-70b-chat-hf").exclude(level="ERROR").filter(metadata__version__icontains="0.0.2").filter(name="business_value_w_format_validation")
 def observation_list(request):
-    # Retrieve all observations from the Observation model
-    observations = Observation.objects.all().filter(model="meta-llama/Llama-3-70b-chat-hf").exclude(level="ERROR").filter(metadata__version__icontains="0.0.2").filter(name="business_value_w_format_validation")
+    observations = (
+        Observation.objects.order_by("created_at")
+        .filter(model="meta-llama/Llama-3-70b-chat-hf")
+        .exclude(level="ERROR")
+        .filter(metadata__version__icontains="0.0.2")
+        .filter(name="business_value_w_format_validation")
+    )  # Order by a unique field
 
-    # Create a Paginator object with the observations and the desired number of items per page
-    paginator = Paginator(observations, 4)
+    paginator = Paginator(observations, 2)
+    page_number = request.GET.get("page")
 
-    # Get the current page number from the request's GET parameters
-    page_number = request.GET.get('page')
+    if page_number == "last":
+        page_number = paginator.num_pages
 
-    # Get the Page object for the current page
     page_obj = paginator.get_page(page_number)
-    for observation in page_obj:
+
+    # Parse the JSON data for each observation
+    for observation in page_obj.object_list:
+        #print(observation.output['content'])
         observation.content_data = json.loads(observation.output['content'])
-    # Render the template with the page object
-    return render(request, 'observation_list.html', {'page_obj': page_obj})
+
+    context = {
+        "page_obj": page_obj,
+    }
+
+    if page_obj.has_next():
+        context["next_url"] = f"?page={page_obj.next_page_number()}"
+
+    if page_obj.has_previous():
+        context["previous_url"] = f"?page={page_obj.previous_page_number()}"
+
+    return render(request, "observation_list.html", context)
